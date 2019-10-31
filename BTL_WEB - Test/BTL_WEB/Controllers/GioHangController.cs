d01157ddf31c90a41d1c9b408a26db5a0b0b4d61
asdcxsd <nguyenmanhthien98@gmail.com>
2019-10-31 18:09:41 +0700

    giohang

--- BTL_WEB - Test/BTL_WEB/Controllers/GioHangController.cs
+++ BTL_WEB - Test/BTL_WEB/Controllers/GioHangController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using BTL_WEB.Models.Functions;
+using BTL_WEB.Models.Entities;
+using BTL_WEB.Code;
 namespace BTL_WEB.Controllers
 {
     public class GioHangController : Controller
@@ -11,6 +13,14 @@ namespace BTL_WEB.Controllers
         // GET: GioHang
         public ActionResult Index()
         {
+            if (Session["usernane"] == null)
+            {
+                return Redirect("~/admin/Login/Index");
+            }
+            Func_TaiKhoan  getid = new Func_TaiKhoan();
+            UserLogin user = (UserLogin)Session["username"];
+            tbl_taikhoan tk = getid.getTaiKhoan(user.tentaikhoan);
+            ViewBag.iduser = tk.id;
             return View();
         }
     }
---
+++ BTL_WEB - Test/BTL_WEB/Models/Functions/Func_TaiKhoan.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BTL_WEB.Models.Entities;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using BTL_WEB.Models;
+
+namespace BTL_WEB.Models.Functions
+{
+    public class Func_TaiKhoan
+    {
+        private MyDBContext context;
+        public Func_TaiKhoan()
+        {
+            context = new MyDBContext();
+        }
+        public IQueryable<tbl_taikhoan> DS_TaiKhoan
+        {
+            get { return context.tbl_taikhoan; }
+        }
+
+        // Trả về 1 đối tượng khi biết khóa
+        public tbl_taikhoan FindEntity(int id)
+        {
+            tbl_taikhoan dbEntry = context.tbl_taikhoan.Find(id);
+            return dbEntry;
+        }
+
+        // Thêm 1 đối tượng
+        public int? Insert(tbl_taikhoan model)
+        {
+            tbl_chitietdonhang dbEntry = context.tbl_chitietdonhang.Find(model.id);
+            if (dbEntry != null)
+            {
+                return null;
+            }
+
+            context.tbl_taikhoan.Add(model);
+            context.SaveChanges();
+            return model.id;
+        }
+
+        // Sửa dữ liệu
+        public int? Update(tbl_taikhoan model)
+        {
+            tbl_taikhoan dbEntry = context.tbl_taikhoan.Find(model.id);
+            if (dbEntry == null)
+            {
+                return null;
+            }
+
+            dbEntry.id_q = model.id_q;
+            dbEntry.id_ttcn = model.id_ttcn;
+            dbEntry.matkhau = model.matkhau;
+            dbEntry.tentaikhoan = model.tentaikhoan;
+            dbEntry.trangthai = model.trangthai;
+            
+            context.SaveChanges();
+
+            return model.id;
+        }
+
+        // Xóa theo key
+        public int? Delete(int id)
+        {
+            tbl_taikhoan dbEntry = context.tbl_taikhoan.Find(id);
+            if (dbEntry == null)
+            {
+                return null;
+            }
+
+            context.tbl_taikhoan.Remove(dbEntry);
+            context.SaveChanges();
+            return id;
+        }
+
+        public tbl_taikhoan getTaiKhoan(string name)
+        {
+            List<tbl_taikhoan> res = context.tbl_taikhoan.Where(x => x.tentaikhoan == name).ToList();
+            if (res.Count >= 1)
+                return res[0];
+            else return null;
+        }
+
+
+    }
+}
