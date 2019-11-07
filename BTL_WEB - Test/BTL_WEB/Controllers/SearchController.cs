ef9f6c14c02587effc454fd1f575b01d0799dc90
Hoangthuy125 <thuy94.mta@gmail.com>
2019-11-07 09:42:44 +0700

    thuy

---
+++ BTL_WEB - Test/BTL_WEB/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+
+namespace BTL_WEB.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
